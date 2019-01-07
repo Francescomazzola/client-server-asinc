7c245ec586e6ce4813fe288135c45bf091f05e04
FreeTax <francesco.mazzola00@gmail.com>
2019-01-07 18:57:16 +0100

    correct little thinks

--- server/server/Origine.cpp
+++ server/server/Origine.cpp
@@ -16,8 +16,6 @@ void chatting(SOCKET	listenSocket) {
 	
 	sin_size = sizeof(struct sockaddr_in);
 	remoteSocket = accept(listenSocket, (struct sockaddr *)&Client_addr, &sin_size);
-
-	/* Il Server accetta il messaggio dal Client */
 	recv(remoteSocket, buffer, sizeof(buffer), 0);
 	if (strcmp(buffer, "q") == 0)
 		return;
@@ -34,21 +32,16 @@ int main()
 	SOCKADDR_IN Server_addr;
 	short port;
 	int wsastartup;
-	
 
-	/* Inizializzazione della libreria Socket */
 	WORD wVersionRequested = MAKEWORD(2, 2);
 	WSADATA wsaData;
 	wsastartup = WSAStartup(wVersionRequested, &wsaData);
-	if (wsastartup != NO_ERROR) printf("Errore WSAStartup()\n");
+	if (wsastartup != NO_ERROR) cout << "Errore WSAStartup()\n" << endl;
 
-	/* Creazione della Socket che si porrà in ascolto di richieste del Client*/
 	listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (listenSocket < 0)
-		printf("Server: errore nella creazione della socket.\n");
-	else printf("La Listening Socket è partita\n");
+	if (listenSocket < 0) cout << "Server: errore nella creazione della socket.\n" << endl;
+	else cout << "La Listening Socket è partita\n" << endl;
 
-	/* Effettua la bind sull’indirizzo e porta ora specificati */
 	port = 3456;
 
 	Server_addr.sin_family = AF_INET;
@@ -56,16 +49,15 @@ int main()
 	Server_addr.sin_port = htons(port);
 
 	if (bind(listenSocket, (LPSOCKADDR)&Server_addr, sizeof(struct sockaddr)) < 0)
-		printf("Server: errore durante la bind.\n");
+		cout << "Server: errore durante la bind.\n" << endl;
 
-	/* La socket si pone in "ascolto" tramite la listen() */
 	ls_result = listen(listenSocket, SOMAXCONN);
 	if (ls_result < 0) printf("Server: errore durante la listen.\n");
-	else printf("La Socket è in Ascolto\n");
+	else cout << "La Socket è in Ascolto\n" << endl;
 
 	chatting( listenSocket);
 
-	printf("Chiudo il Server");
+	cout << "Chiudo il Server" << endl;
 	closesocket(remoteSocket);
 	WSACleanup();
 	system("pause");
