39acf934e231e6120803cf5f12b111690a3d82cd
LapoMeucci <lapomeucci@gmail.com>
2019-01-08 09:36:24 +0100

    inserting print ip addr and port of client and server

--- client/client/Origine.cpp
+++ client/client/Origine.cpp
@@ -12,15 +12,10 @@ void chatting() {
 	char messaggio[80];
 	char buffer[1024] = { 0 };
 
-	if (connect(clientsocket, (LPSOCKADDR)&addr, sizeof(addr)) < 0) {
-		cout << "errore!" << endl;
-		return;
-	}
-
 	cout << "dammi il messaggio da fornire al server, per chiudere premere q" << endl;
 	cin >> messaggio;
 	send(clientsocket, messaggio, sizeof(messaggio), 0);
-	if (messaggio == "q")
+	if (strcmp(messaggio,"q")==0)
 		return;
 	if (recv(clientsocket, buffer, 2000, 0) > 0) {
 		cout << buffer << endl;
@@ -37,11 +32,15 @@ int main()
 	WORD wVersionRequested = MAKEWORD(2, 2);
 	WSADATA wsaData;
 	WSAStartup(wVersionRequested, &wsaData);
-	port = 3456;
+	port = 27000;
 	addr.sin_family = AF_INET;
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	addr.sin_port = htons(port);
 	clientsocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (connect(clientsocket, (LPSOCKADDR)&addr, sizeof(addr)) < 0) {
+		cout << "errore!" << endl;
+		exit(1);
+	}
 	chatting();
 	closesocket(clientsocket);
 	WSACleanup();
--- server/server/Origine.cpp
+++ server/server/Origine.cpp
@@ -14,8 +14,7 @@ int sin_size;
 void chatting(SOCKET	listenSocket) {
 	char buffer[256];
 	
-	sin_size = sizeof(struct sockaddr_in);
-	remoteSocket = accept(listenSocket, (struct sockaddr *)&Client_addr, &sin_size);
+	
 	recv(remoteSocket, buffer, sizeof(buffer), 0);
 	if (strcmp(buffer, "q") == 0)
 		return;
@@ -42,7 +41,7 @@ int main()
 	if (listenSocket < 0) cout << "Server: errore nella creazione della socket.\n" << endl;
 	else cout << "La Listening Socket è partita\n" << endl;
 
-	port = 3456;
+	port = 27000;
 
 	Server_addr.sin_family = AF_INET;
 	Server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -55,12 +54,16 @@ int main()
 	if (ls_result < 0) printf("Server: errore durante la listen.\n");
 	else cout << "La Socket è in Ascolto\n" << endl;
 
+	sin_size = sizeof(struct sockaddr_in);
+	remoteSocket = accept(listenSocket, (struct sockaddr *)&Client_addr, &sin_size);
+
+	cout << "client " << inet_ntoa(Client_addr.sin_addr) << "  " << ntohs(Client_addr.sin_port) << endl;
+	cout << "server " << inet_ntoa(Server_addr.sin_addr) << " " << ntohs(Server_addr.sin_port) << endl;
 	chatting( listenSocket);
 
 	cout << "Chiudo il Server" << endl;
 	closesocket(remoteSocket);
 	WSACleanup();
-	system("pause");
 	return 0;
 }
 
