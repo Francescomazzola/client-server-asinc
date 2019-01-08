b305edb182777ba1d19995ab99faa33b0509f9be
LapoMeucci <lapomeucci@gmail.com>
2019-01-08 09:15:01 +0100

    i'v correct some error and i'v optimizate my code

--- client/client/Origine.cpp
+++ client/client/Origine.cpp
@@ -5,15 +5,35 @@
 #pragma comment (lib,"Ws2_32.lib")
 
 using namespace std;
+SOCKET	clientsocket;
+SOCKADDR_IN addr;
+
+void chatting() {
+	char messaggio[80];
+	char buffer[1024] = { 0 };
+
+	if (connect(clientsocket, (LPSOCKADDR)&addr, sizeof(addr)) < 0) {
+		cout << "errore!" << endl;
+		return;
+	}
+
+	cout << "dammi il messaggio da fornire al server, per chiudere premere q" << endl;
+	cin >> messaggio;
+	send(clientsocket, messaggio, sizeof(messaggio), 0);
+	if (messaggio == "q")
+		return;
+	if (recv(clientsocket, buffer, 2000, 0) > 0) {
+		cout << buffer << endl;
+	}
+	chatting();
+
+}
 
 int main()
 {
 	int valread;
-	SOCKET	clientsocket;
-	SOCKADDR_IN addr;
-	char messaggio[80];
 	short port;
-	char buffer[1024] = {0};
+	
 	WORD wVersionRequested = MAKEWORD(2, 2);
 	WSADATA wsaData;
 	WSAStartup(wVersionRequested, &wsaData);
@@ -22,22 +42,8 @@ int main()
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	addr.sin_port = htons(port);
 	clientsocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-	if (connect(clientsocket, (LPSOCKADDR)&addr, sizeof(addr)) < 0) {
-		cout << "errore!" << endl;
-		exit(1);
-	}
-		
-	cout << "dammi il messaggio da fornire al server, per chiudere premere q" << endl;
-	cin >> messaggio;
-	send(clientsocket, messaggio, sizeof(messaggio), 0);
-	if (messaggio == "q")
-		exit(1);
-	if (recv(clientsocket, buffer, 2000, 0) > 0) {
-		cout << buffer<<endl;
-	}
-	main();
-
+	chatting();
+	closesocket(clientsocket);
 	WSACleanup();
 	
 
